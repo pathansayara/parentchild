092a45762360602304fde2d79f0db26d7b60bb95
sayara <sayarapathan2431@gmail.com>
2023-10-14 13:01:04 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'parenttochild';
+  name='sayara pathan';
+  actor='Madhuri Dixit';
+  obj={
+    name: "kumar",
+    age:23,
+    isMarried:true
+  }
+ 
+  fruits=["Apple","Banana","Mango","Grapes"];
+  changeData(){
+    this.name='Sayara is good girl'
+  }
+
 }
